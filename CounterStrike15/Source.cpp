a83991914f064f1a6b7aecbe846d68c1c7a09177
Hephlin <AbagaleHephlin@gmail.com>
2025-07-02 08:13:16 -0500

    Update Source.cpp

--- CounterStrike15/Source.cpp
+++ CounterStrike15/Source.cpp
@@ -1,1 +1,13 @@
+#include <stdbool>
+#include <memory>
+#include <Windows.h>
+#include <iostream>
+#include <vector.h>
+#include <string.h> 
+
+#include "hacks/Lazer/Lazer.hpp>
+#include "hacks/Lazer/Lazer.cpp>
+
+#include <Signature.hpp>
+#include <Signature.cpp>
 
