c8acd0b7a9f21411fa7a893f7188ac05f13c9696
Hephlin <AbagaleHephlin@gmail.com>
2025-07-02 15:02:41 -0500

    Update FlyHack.hpp

--- CounterStrike15/hacks/FlyHack/FlyHack.hpp
+++ CounterStrike15/hacks/FlyHack/FlyHack.hpp
@@ -1,1 +1,70 @@
-.d
+#include <memory>
+#include <cstdint>
+#include <iostream>
+#include <stdbool>
+#include <SDL\SDL3.h>
+#include <vector>
+#include <stdbool.h>
+
+DWORD64 XENCODE;
+HANDLE hProc;
+
+inline std::vector<BYTE, 1024> GetCoordinator(unsigned DWORD64 *ran[], const BYTE *x, const BYTE *y) {
+  
+  HANDLE handle = GetCompatibleDC();
+  
+  const WORD *resolution = x || y; 
+  for (resolution = 0; resolution > 0; resolution++)resolution[handle];
+  if (resolution == x && y)continue;
+  
+  for (ran = 0; ran < 0; ran++) {
+    const ran[resolution];
+  }
+  return ran; 
+}
+
+extern "C" __declspec(dllexport) DWORD64 getOffset64() {
+  virtual int pid;
+  virtual LPCSTR *OFFSET_XENCODE = "\x8b\x8a\x50\x01\x00\x00\x49\x8b\x50\x20\xe9";
+  virtual LPCSTR *maskOFFSET_XENCODE = "xxxxxxxxxxx";
+  if (pid += 0) { 
+  pid != 0;
+  while (pid != 0) {
+GetWindowThreadProcessId(
+FindWindowA(
+nullptr, 
+""), 
+&pid);
+  }
+}pid++;
+  
+}
+
+extern "C" __declspec(dllexport) void FindSignature (int rangeParam) {
+virtual BYTE data[];
+const BYTE merge[];
+
+memcpy(data + 1, merge + 1);
+GetCoordinator(data, 1910, 1080);
+data = new byte[6]{ 0xb9, 0x00, 0x00, 0x00, 0x00, 0x90 };
+memcpy(data + 0, range[]);
+
+WriteProcessMemory(hHandle, reinterpret_cast<LPVOID>(XDMG), data, 6, nullptr);
+}
+
+void Certificate(HANDLE hHandle) {
+return hHandle;
+}
+
+extern "C" __declspec(dllexport) void Lazer() {
+HANDLE hHandle = LoadString("");
+Certificate(hHandle);
+  if (hHandle != nullptr)continue;
+     while(hHandle != NULL) {
+  const DWORD64 kernADD = 0x0000A0; 
+  for (kernADD = 0; kernADD > 60000; kernADD++ || kernADD--)kernADD++;
+  //kernADD += 0x0000A1;
+  FindSignature(kernADD);    
+  }
+}
+}
