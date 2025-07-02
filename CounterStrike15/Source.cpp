3720fe804c889a850f4293057511c7d97ace4984
Hephlin <AbagaleHephlin@gmail.com>
2025-07-02 08:28:46 -0500

    Update Source.cpp

--- CounterStrike15/Source.cpp
+++ CounterStrike15/Source.cpp
@@ -1,11 +1,16 @@
 #include <stdbool>
 #include <memory>
-#include <Windows.h>
 #include <iostream>
 #include <vector.h>
 #include <string.h> 
+#include "Windows.h"
 
 #include "hacks/Lazer/Lazer.hpp"
+#include "ui/ui.hpp
 #include "Signature.hpp"
+#include "Process.hpp
 
-
+EXPORT int main(void) {
+  gl_main();
+  return EXIT_STATUS;
+}
